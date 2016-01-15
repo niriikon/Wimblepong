836ecb871303525b977740c783bb0c7aa4a831b1
Uula Ranta <uula.ranta@gmail.com>
2016-01-15 02:10:34 +0200

    FIXEDED STUPID BUG

--- wimblepong.py
+++ wimblepong.py
@@ -4,7 +4,7 @@ import json
 import logging
 import socket
 import sys
-import PongGame
+import Pong
 
 X_LEFT = 0
 X_RIGHT = 640
@@ -33,15 +33,16 @@ class PingPongBot(object):
         self._connection = connection
         self._log = log
 
-    def run(self, teamname):
+    def run(self, teamname, oppname=None):
         self.name = teamname
-        self.game = PongGame(self._log, teamname)
-        self._connection.send({'msgType': 'join', 'data': teamname})
-        self._response_loop()
+        self._log.info("Starting game!")
+        if oppname is None:
+            self.game = Pong.PongGame(self._log, teamname)
+            self._connection.send({'msgType': 'join', 'data': teamname})
+        else:
+            self.game = Pong.PongGame(self._log, teamname, oppname)
+            self._connection.send({'msgType': 'requestDuel', 'data': [teamname, oppname]})
 
-    def runvs(self, teamname, oppname):
-        self.game = PongGame(self._log, teamname)
-        self._connection.send({'msgType': 'requestDuel', 'data': [teamname, oppname]})
         self._response_loop()
 
     def _response_loop(self):
@@ -69,15 +70,19 @@ class PingPongBot(object):
         ball_y = 0
         plyr_y = 0
         paddle_mid = 25
+
         try:
             self.game.update(data)
-            ball_y = self.game.ball.projected_y(side)
+            ball_y = self.game.ball.projected_y(self.game.me.side)
             plyr_y = self.game.me.y
             paddle_mid = self.game.conf.paddleHeight / 2
         except KeyError:
             self._log.error('Object not found in json')
+
         dir = 0
-        if ball_y > (plyr_y + paddle_mid):
+        if abs(ball_y - plyr_y - paddle_mid) < 1:
+            dir = 0
+        elif ball_y > (plyr_y + paddle_mid):
             dir = 1.0
         elif ball_y < (plyr_y + paddle_mid):
             dir = -1.0
@@ -96,10 +101,12 @@ if __name__ == '__main__':
             teamname, hostname, port = sys.argv[1:]
             PingPongBot(JsonOverTcp(hostname, port), log).run(teamname)
         except TypeError:
+            raise
             sys.exit(__doc__)
     else:
         try:
             teamname, oppname, hostname, port = sys.argv[1:]
-            PingPongBot(JsonOverTcp(hostname, port), log).runvs(teamname, oppname)
+            PingPongBot(JsonOverTcp(hostname, port), log).run(teamname, oppname)
         except TypeError:
+            raise
             sys.exit(__doc__)
